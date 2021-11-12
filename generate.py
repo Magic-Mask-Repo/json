eed944ea4a70399cee5eae3b565b1913aa5d5d08
Burak Mete Erdoğan <bugrevealingbme@gmail.com>
2021-11-12 21:40:17 +0300

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,10 +1,33 @@
 import sys
 import json
 from github import Github
+import requests
+
 
 # Configuration
 REPO_NAME = "Magic-Mask-Repo"
 REPO_TITLE = "Magic Mask"
+
+
+def json_parse(text):
+    json_data = {}
+    liste = text.split("\n")
+    for elem in liste:
+        if elem:
+            key = elem.split("=")[0]
+            value = elem.split("=")[1]
+            json_data[key] = value
+    return json_data
+
+
+
+
+def get_module_prop_content(repo):
+    try:
+        res = requests.get(f'https://raw.githubusercontent.com/{repo.full_name}/master/module.prop',headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'})
+        return json_parse(res.text)
+    except:
+        return None
 
 # Skeleton for the repository
 meta = {
@@ -33,10 +56,12 @@ for repo in repos:
             moduleprop[lhs] = rhs
         
         # Create meta module information
+
         module = {
             "id": moduleprop["id"],
             "last_update": int(repo.updated_at.timestamp() * 1000),
             "prop_url": f"https://raw.githubusercontent.com/{repo.full_name}/master/module.prop",
+            "prop_content": get_module_prop_content(repo),
             "zip_url": f"https://github.com/{repo.full_name}/archive/master.zip",
             "notes_url": f"https://raw.githubusercontent.com/{repo.full_name}/master/README.md"
         }
